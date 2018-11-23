4c855ef4d5ca0a25932da489e4bb566d7728eedf
Brian Beal <brianwbeal@gmail.com>
2018-11-23 11:17:26 -0800

    create variables for the api key and location (city ID) of user. write shell for request to the open weather api, passing in an object containing three key-value pairs. the url of the api we will be making the request to, a function to call if the request is successful, and a function to call if we get an error. Using jquery to send the http request.

--- scripts.js
+++ scripts.js
@@ -1,7 +1,16 @@
-
-
 $(document).ready(() => {
+
+  // api key
+  const apiKey = "";
+  const cityID = "5746545";  // portland, or, usa
+
   $.get({
-    
+    url: `http://api.openweathermap.org/data/2.5/weather?id=${cityID}&APPID=${apiKey}&units=imperial`,
+    success: (result) => {
+      console.log(result);
+    },
+    error: (error) => {
+      console.log(error);
+    }
   });
 });
