4270eb50b39535dba7d74c9c5df3204dc812e780
Brian Beal <brianwbeal@gmail.com>
2018-11-23 11:28:03 -0800

    store first value from api and comment out sections for the rest of the variable values. add forecast section to markup.

--- scripts.js
+++ scripts.js
@@ -8,6 +8,31 @@ $(document).ready(() => {
     url: `http://api.openweathermap.org/data/2.5/weather?id=${cityID}&APPID=${apiKey}&units=imperial`,
     success: (result) => {
       console.log(result);
+
+      // city
+      const currentCity = result.name;
+      console.log(currentCity);
+
+      // time
+
+      // icon
+
+      // temperature
+
+      // description
+
+      // sunrise time
+
+      // sunset time
+
+      // wind
+
+      // high temperature
+
+      // low temperature
+
+      // forecast
+
     },
     error: (error) => {
       console.log(error);
